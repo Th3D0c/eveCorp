03cda5ef6f182318352dd4933a8ceeeeb151b3eb
Th3D0c <Th3D0c@thedochouse.org>
2011-03-17 01:06:59 +0100

    Remise en place de EAAL apres discution avec le coder (pp), ca marche du tonnere !!! :)

--- app/controllers/perso_controller.rb
+++ app/controllers/perso_controller.rb
@@ -52,61 +52,81 @@ class PersoController < ApplicationController
 	end
 	
 	def index
-		content = Net::HTTP.post_form(URI.parse("http://api.eveonline.com/char/CharacterSheet.xml.aspx"), 
-		{'apiKey' => '39EB48CB131C4D5792C170FE120B35CC008F513C95DE470397629609981563E6', 'characterID' => '90501427', 'userID' => '7518753'}	)
-
-		xmldoc = Document.new(content.body) 
 	
-		@@charac =  Hash.new
-		@@charac['skillsPoints'] = 0
-		xmldoc.elements.each('eveapi/result/*') { |zeElement|
-			puts zeElement
-			puts zeElement.name
-			puts zeElement.text
-			puts zeElement.has_elements?
-			puts zeElement.has_attributes?
-			puts zeElement.attributes["name"]
-			if (zeElement.has_elements? == false && zeElement.has_attributes? == false)
-				@@charac[zeElement.name] = zeElement.text.to_s
-			elsif (zeElement.has_elements? == true && zeElement.has_attributes? == false)
-				
-								
-				puts OpenStruct.new recurse(zeElement)				
-				@@charac[zeElement.name] = OpenStruct.new recurse(zeElement)
-			elsif (zeElement.name == 'rowset')
-				puts recurse2(zeElement)
-				@@charac[zeElement.attributes['name']] = recurse2(zeElement)
+	
+	require 'eaal'
+	EAAL.cache = EAAL::Cache::FileCache.new               # set cache
+	api = EAAL::API.new("7518753", "39EB48CB131C4D5792C170FE120B35CC008F513C95DE470397629609981563E6")        # initialize API object
+	api.scope = "char"                                    # set scope to "char"
+	@characterSheat = api.CharacterSheet("characterID" => 90501427)    # request Killlog API Page
+	
+	
+	api.scope = "eve"                                    # set scope to "char"
+	@skillTree = api.SkillTree()    
+	puts @skillTree.skillGroups.first.skills.first.inspect
+#	puts @charac.inspect
+	
+	
+	
+#	return 0
+	
+	
+#	
+#		content = Net::HTTP.post_form(URI.parse("http://api.eveonline.com/char/CharacterSheet.xml.aspx"), 
+#		{'apiKey' => '39EB48CB131C4D5792C170FE120B35CC008F513C95DE470397629609981563E6', 'characterID' => '90501427', 'userID' => '7518753'}	)
+#
+#		xmldoc = Document.new(content.body) 
+#	
+#		@@charac =  Hash.new
+#		@@charac['skillsPoints'] = 0
+#		xmldoc.elements.each('eveapi/result/*') { |zeElement|
+#			puts zeElement
+#			puts zeElement.name
+#			puts zeElement.text
+#			puts zeElement.has_elements?
+#			puts zeElement.has_attributes?
+#			puts zeElement.attributes["name"]
+#			if (zeElement.has_elements? == false && zeElement.has_attributes? == false)
+#				@@charac[zeElement.name] = zeElement.text.to_s
+#			elsif (zeElement.has_elements? == true && zeElement.has_attributes? == false)
 #				
-#				buffer =  Hash.new		
-#				if (zeElement.has_elements? == true)
-#					att_list = zeElement.attributes["columns"].split(',')
-#					att_key = zeElement.attributes["key"].to_s
-##					att_list.delete('unpublished')		# Hack characterSheat
-##					puts att_list
-#					
-#					zeElement.elements.each('*') { |zeElementType1|
-#						buffer1 =  Hash.new
-#						att_list.each { |att| 
-##							puts zeElementType1.attributes[att]
-#							if(att == 'unpublished' && zeElementType1.attributes[att].nil? == false)
-#								buffer1['level'] = zeElementType1.attributes[att] 
-#							else
-#								buffer1[att] = zeElementType1.attributes[att]
-#							end
-#							
-#						}
-#						buffer[zeElement.attributes['name']+buffer1[att_key]] = OpenStruct.new buffer1
-#						
-#						if(zeElement.attributes['name'] == 'skills')
-##							puts buffer[zeElement.attributes['name']+buffer1[att_key]]
-#							charac['skillsPoints'] += buffer[zeElement.attributes['name']+buffer1[att_key]].skillpoints.to_i
-#						end
-#					}
-#				end
-#				charac[zeElement.attributes['name']] = OpenStruct.new buffer
-			end
-		}
-		@characterSheat = OpenStruct.new @@charac
+#								
+#				puts OpenStruct.new recurse(zeElement)				
+#				@@charac[zeElement.name] = OpenStruct.new recurse(zeElement)
+#			elsif (zeElement.name == 'rowset')
+#				puts recurse2(zeElement)
+#				@@charac[zeElement.attributes['name']] = recurse2(zeElement)
+##				
+##				buffer =  Hash.new		
+##				if (zeElement.has_elements? == true)
+##					att_list = zeElement.attributes["columns"].split(',')
+##					att_key = zeElement.attributes["key"].to_s
+###					att_list.delete('unpublished')		# Hack characterSheat
+###					puts att_list
+##					
+##					zeElement.elements.each('*') { |zeElementType1|
+##						buffer1 =  Hash.new
+##						att_list.each { |att| 
+###							puts zeElementType1.attributes[att]
+##							if(att == 'unpublished' && zeElementType1.attributes[att].nil? == false)
+##								buffer1['level'] = zeElementType1.attributes[att] 
+##							else
+##								buffer1[att] = zeElementType1.attributes[att]
+##							end
+##							
+##						}
+##						buffer[zeElement.attributes['name']+buffer1[att_key]] = OpenStruct.new buffer1
+##						
+##						if(zeElement.attributes['name'] == 'skills')
+###							puts buffer[zeElement.attributes['name']+buffer1[att_key]]
+##							charac['skillsPoints'] += buffer[zeElement.attributes['name']+buffer1[att_key]].skillpoints.to_i
+##						end
+##					}
+##				end
+##				charac[zeElement.attributes['name']] = OpenStruct.new buffer
+#			end
+#		}
+#		@characterSheat = OpenStruct.new @@charac
 		puts "\n\n\n===============================\n\n\n"
 		puts @characterSheat
 	end
